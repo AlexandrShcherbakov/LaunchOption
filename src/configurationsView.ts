3d5e3f2a09cd1a846203f9a0bd62ff865031bfd1
a.shcherbakov <a.shcherbakov@gaijin.ru>
2020-12-18 14:36:26 +0300

    Support filters for possible option values based on current options. We should provide a list of possible values for each filter.

--- src/configurationsView.ts
+++ src/configurationsView.ts
@@ -27,11 +27,12 @@ export class ConfigViewProvider implements vscode.TreeDataProvider<ConfigOption>
 				{
 					if ((values[i] instanceof Object))
 					{
-						values[i] = new OptionPair(values[i]["name"], values[i]["value"]);
+						let filter = values[i].hasOwnProperty("filter") ? values[i]["filter"] : [];
+						values[i] = new OptionPair(values[i]["name"], values[i]["value"], filter);
 					}
 					else
 					{
-						values[i] = new OptionPair(values[i], values[i]);
+						values[i] = new OptionPair(values[i], values[i], []);
 					}
 				}
 				let currentValue;
@@ -64,6 +65,10 @@ export class ConfigViewProvider implements vscode.TreeDataProvider<ConfigOption>
         return Promise.resolve(this.options);
 	}
 
+	getOptions() : ConfigOption[] {
+		return this.options;
+	}
+
     refresh(): void {
 		this._onDidChangeTreeData.fire(null);
 	}
@@ -85,8 +90,12 @@ export class ConfigTreeOptions implements vscode.TreeViewOptions<ConfigOption> {
 
 const EmptyRow : string = "\"empty\"";
 
+class OptionFilter {
+	constructor(public name : string, public values : Array<string>) {}
+}
+
 class OptionPair {
-	constructor(public name : string, public value : string) {}
+	constructor(public name : string, public value : string, public filters : Array<OptionFilter>) {}
 }
 export class ConfigOption extends vscode.TreeItem {
 	tooltip : string;
@@ -103,10 +112,26 @@ export class ConfigOption extends vscode.TreeItem {
 		this.description = this.value.name == "" ? EmptyRow : this.value.name;
 	}
 
-    public changeConfigParam(provider: ConfigViewProvider, context: vscode.ExtensionContext) {
+	private genValuesToPick(provider: ConfigViewProvider) {
 		let valuesToPick : Array<string> = Array<string>();
-		for (let i = 0; i < this.values.length; ++i)
-			valuesToPick.push(this.values[i].name.length == 0 ? EmptyRow : this.values[i].name);
+		let currentOptions : ConfigOption[] = provider.getOptions();
+		for (let i = 0; i < this.values.length; ++i) {
+			let filterPassed :boolean = true;
+			for (let opt of currentOptions) {
+				let filterIdx = this.values[i].filters.findIndex((filt: OptionFilter) => {return opt.label == filt.name;});
+				let optionPassed : boolean = filterIdx == -1;
+				filterPassed = filterPassed &&
+				(optionPassed || this.values[i].filters[filterIdx].values.findIndex((value :string) => {return opt.value.name == value;}) != -1);
+			}
+			if (filterPassed) {
+				valuesToPick.push(this.values[i].name.length == 0 ? EmptyRow : this.values[i].name);
+			}
+		}
+		return valuesToPick;
+	}
+
+    public changeConfigParam(provider: ConfigViewProvider, context: vscode.ExtensionContext) {
+		let valuesToPick : Array<string> = this.genValuesToPick(provider);
 		let lineValue = this.value.name == "" ? EmptyRow : this.value.name;
 		valuesToPick.splice(valuesToPick.indexOf(lineValue), 1);
 		valuesToPick.unshift(lineValue);
